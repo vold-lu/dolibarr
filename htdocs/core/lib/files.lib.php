97af0975bc710943374ab1b57b34efee40337c03
Rémi Champlon <r.champlon@vold.lu>
2025-01-27 11:58:33 +0100

    indent

--- htdocs/core/lib/files.lib.php
+++ htdocs/core/lib/files.lib.php
@@ -3565,17 +3565,17 @@ function dol_check_secure_access_document($modulepart, $original_file, $entity, 
 				exit;
 			}
 
-            // Check fuser->rights->modulepart->myobject->read and fuser->rights->modulepart->read
-            $partsofdirinoriginalfile = explode('/', $original_file);
-            if (!empty($partsofdirinoriginalfile[1])) {	// If original_file is xxx/filename (xxx is a part we will use)
-                $partofdirinoriginalfile = $partsofdirinoriginalfile[0];
-                if (($partofdirinoriginalfile && ($fuser->hasRight($tmpmodule, $partofdirinoriginalfile, 'lire') || $fuser->hasRight($tmpmodule, $partofdirinoriginalfile, 'read'))) || preg_match('/^specimen/i', $original_file)) {
-                    $accessallowed = 1;
-                }
-            }
-            if (($fuser->hasRight($tmpmodule, $lire) || $fuser->hasRight($tmpmodule, $read)) || preg_match('/^specimen/i', $original_file)) {
-                $accessallowed = 1;
-            }
+			// Check fuser->rights->modulepart->myobject->read and fuser->rights->modulepart->read
+			$partsofdirinoriginalfile = explode('/', $original_file);
+			if (!empty($partsofdirinoriginalfile[1])) {    // If original_file is xxx/filename (xxx is a part we will use)
+				$partofdirinoriginalfile = $partsofdirinoriginalfile[0];
+				if (($partofdirinoriginalfile && ($fuser->hasRight($tmpmodule, $partofdirinoriginalfile, 'lire') || $fuser->hasRight($tmpmodule, $partofdirinoriginalfile, 'read'))) || preg_match('/^specimen/i', $original_file)) {
+					$accessallowed = 1;
+				}
+			}
+			if (($fuser->hasRight($tmpmodule, $lire) || $fuser->hasRight($tmpmodule, $read)) || preg_match('/^specimen/i', $original_file)) {
+				$accessallowed = 1;
+			}
 
 			$original_file = $conf->$tmpmodule->dir_output.'/temp/massgeneration/'.$user->id.'/'.$original_file;
 		} else {
